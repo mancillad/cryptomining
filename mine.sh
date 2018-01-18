7c4073c82c439038f256595ed60af95b6889403e
mancillad <mancillad@users.noreply.github.com>
2018-01-18 08:12:24 +0800

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,1 +1,1 @@
-./xmrig -a cryptonight -o stratum+tcp://etn.easyhash.io:3630:5000 -u etnkMRVwib4VSHZqw2Aq4MWgQn9KV5Bb7KJpuYUwCnpo8sEpbFYPh946yej1tDBGTYJrj9tPxU7akE7RYeos8z9A9Ajis8QcV1 -p x
+screen ./xmrig -a cryptonight -o stratum+tcp://etn.easyhash.io:3630:5000 -u etnkMRVwib4VSHZqw2Aq4MWgQn9KV5Bb7KJpuYUwCnpo8sEpbFYPh946yej1tDBGTYJrj9tPxU7akE7RYeos8z9A9Ajis8QcV1 -p x
