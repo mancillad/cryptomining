e9a560ba652438e92be6247a63d952dcf751cebe
mancillad <mancillad@users.noreply.github.com>
2018-01-23 11:41:11 +0800

    Create mine-fairpool.sh

---
+++ mine-fairpool.sh
@@ -0,0 +1,1 @@
+/root/projectx/xmrig -a cryptonight -o stratum+tcp://mine.etn.fairpool.xyz:8888 -u etnkMRVwib4VSHZqw2Aq4MWgQn9KV5Bb7KJpuYUwCnpo8sEpbFYPh946yej1tDBGTYJrj9tPxU7akE7RYeos8z9A9Ajis8QcV1 -p x
